3f8ca59d45d4a09e5dccc9760dcb43ddc0e74b0a
Darlan Costa <44330939+darlancostadev@users.noreply.github.com>
2022-06-24 00:11:01 -0300

    refactor: removendo comentários desnecessarios

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,8 +1,3 @@
-// import React from 'react'
-// import { render } from '@testing-library/react'
-// import '@testing-library/jest-dom/extend-expect'
-// import Header from '.'
-
 import { render, screen } from '../../../utils/test-utils'
 
 import Header from '.'
@@ -10,12 +5,6 @@ import Header from '.'
 describe('<Header />', () => {
 	it('should render the heading', () => {
 		render(<Header />)
-
 		expect(screen.getByText('Header')).toBeInTheDocument()
 	})
 })
-
-// it('should render Header', () => {
-// 	const { getByText } = render(<Header />)
-// 	expect(getByText('Header')).toBeInTheDocument()
-// })
